078af2883fade59f334d56266c8eed9f47170e80
Vieri05 <128602456+Vieri05@users.noreply.github.com>
2024-01-17 08:52:06 +0100

    Add files via upload

---
+++ esercizio7JSON/progetto7/src/main/java/com/example/Persona.java
@@ -0,0 +1,42 @@
+package com.example;
+
+
+public class Persona {
+    String nome, cognome;
+    int eta;
+
+    public Persona(){
+        
+    }
+
+    public Persona(String nome, String cognome, int eta){
+        
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+}
---
+++ esercizio7JSON/progetto7/src/main/java/com/example/ServerMain.java
@@ -0,0 +1,15 @@
+package com.example;
+
+
+
+public class ServerMain {
+    public static void main(String[] args) {
+
+        Server server = new Server(8000);
+
+        server.connect(); 
+        server.communicate();
+    }
+
+
+}
